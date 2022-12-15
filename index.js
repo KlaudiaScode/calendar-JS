c52bc4441326f37a248270df310e0ed0a68fa3e4
Klaudia Sagan <klaudiasagan.code@gmail.com>
2022-12-15 17:33:15 +0100

    code refactoring and window.location

--- index.js
+++ index.js
@@ -9,33 +9,38 @@ const task = document.querySelector('div.task');
 const input = document.querySelector('input.time');
 
 
+const replaceSaveWithEditEvent = new Event ('replaceSaveWithEdit');
+
+document.addEventListener('replaceSaveWithEdit',function(){
+    const elementSave = document.querySelector('.save');
+    console.log(elementSave);
+    const rodzicSave = elementSave.parentElement;
+    console.log(rodzicSave);
+    rodzicSave.removeChild(elementSave);
+    const buttonEdit = createEditButton();
+    rodzicSave.appendChild(buttonEdit);
+})
+
 const replaceEditWithSaveEvent = new Event('replaceEditWithSave');
 
-//stworzyć nowy event 
-document.addEventListener('replaceEditWithSave', function(){
-    //znaleźć element który ma klasę to_remove
+document.addEventListener('replaceEditWithSave',function(event){
+    console.log(event.lineIndex);
     const el = document.querySelector('.to_remove');
-    console.log(el);
-    //znaleźć rodzica tego elementu
     const rodzic = el.parentElement;
-    console.log(rodzic);
-    //usunąć element z klasą to_remove
     rodzic.removeChild(el);
-    //stworzyć nowego buttona z klasą save
     const buttonSave = document.createElement('button');
     buttonSave.className = 'save';
-    //stworzyć eventListenera co ma się dziać jak ktoś kliknie na save
     buttonSave.addEventListener('click',function(event){
         event.preventDefault();
         console.log('rodzic',rodzic)
-        const timeTask = rodzic.querySelector('#event-time');
-        const span = rodzic.querySelector('#event-content');
+        const timeTask = rodzic.querySelector('.event-time');
+        const span = rodzic.querySelector('.event-content');
         timeTask.contentEditable = false;
-        timeTask.style.backgroundColor = "#4056a1";
+        timeTask.style.backgroundColor = '#4056a1';
         span.contentEditable = false;
-        span.style.backgroundColor = "#4056a1";
+        span.style.backgroundColor = '#4056a1';
         const date = document.getElementById('date').textContent;
-        const currentEvent = JSON.parse(localStorage.getItem(date) || "{}");
+        const currentEvent = JSON.parse(localStorage.getItem(date) || '{}');
         const oldTime = timeTask.dataset.time;
         const newTime = timeTask.textContent;
         console.log(oldTime, newTime, currentEvent);
@@ -48,11 +53,12 @@ document.addEventListener('replaceEditWithSave', function(){
         currentEvent[newTime] = currentTask;
         localStorage.setItem(date, JSON.stringify(currentEvent));
         //wywołać nowy event
+        document.dispatchEvent(replaceSaveWithEditEvent);
     });
-    //dodać buttona do rodzica
     rodzic.appendChild(buttonSave);
 })
 
+//funckja tworzenia daty 
 function createDataSet(year,month,day){
     let currentDate = new Date(year,month,day);
     if(!year&&!month&&!day){
@@ -68,11 +74,11 @@ function createDataSet(year,month,day){
         currentDate,currentDateText,day:currentDay,month:currentMonth,year:currentYear,firstDay,lastDay
     }
 }
-
+//funckja wyświetlania obecnej daty 
 function displayCurrentDate(dateText){
-    document.getElementById("date_clicked").textContent = dateText;
+    document.getElementById('date_clicked').textContent = dateText;
 }
-
+//funckja przeładowania kalendarza
 function rerenderCalendar(event){
     const currentDateString = event.target.dataset.date;
     const currentDate = new Date(currentDateString);
@@ -85,10 +91,11 @@ function rerenderCalendar(event){
 
 buttonNext.addEventListener('click',rerenderCalendar);
 buttonBack.addEventListener('click',rerenderCalendar);
-
+//funkcja wyświetlania liczb dni miesiąca
 function displayNumberDaysInMonth(lastDay) {
     return lastDay.getDate();
 }
+//funkcja wyświetlania dni 
 function displayDays(daysNumber){
     const arrayDaysInMonth = [];
         for (let i = 1; i <= daysNumber; i++){
@@ -96,111 +103,128 @@ function displayDays(daysNumber){
     }
     return arrayDaysInMonth;
 }
+//renderowanie kalendarza
 function renderCalendar(currentYear,currentMonth,currentDay){
     const {month,year,day,lastDay,firstDay,currentDateText} = createDataSet(currentYear,currentMonth,currentDay);
     displayCurrentDate(currentDateText);
     const daysinMonth = displayNumberDaysInMonth(lastDay);
     const daysArray= displayDays(daysinMonth);
     renderEmptyDivs(firstDay);
-
     daysArray.forEach((dayNumber) => {
-        const dayElement = document.createElement("div");
+        const dayElement = document.createElement('div');
         dayElement.textContent = dayNumber;
-        dayElement.className = "single_day proper_day";
+        dayElement.className = 'single_day proper_day';
         if (dayNumber === day){
-            dayElement.className = "single_day proper_day today";
+            dayElement.className = 'single_day proper_day today';
         }
         const date = `${year}-${('0' + (month + 1)).slice(-2)}-${('0' + dayNumber).slice(-2)}`;
         const storageData = localStorage[date];
         if(storageData){
-            dayElement.className = "single_day proper_day deadline";
+            dayElement.className = 'single_day proper_day deadline';
         }
-        dayElement.setAttribute("data-date",date);
+        dayElement.setAttribute('data-date',date);
         singleDayContainer.append(dayElement);
     });
     const nextDate = new Date (year,month+1,day);
-    buttonNext.setAttribute("data-date",`${nextDate.getFullYear()}-${('0'+ (nextDate.getMonth()+1)).slice(-2)}-`
+    buttonNext.setAttribute('data-date',`${nextDate.getFullYear()}-${('0'+ (nextDate.getMonth()+1)).slice(-2)}-`
       +('0'+ nextDate.getDate()).slice(-2));
-
     const backDate = new Date (year,month-1,day);
-    buttonBack.setAttribute("data-date",`${backDate.getFullYear()}-${('0'+ (backDate.getMonth()+1)).slice(-2)}-${('0'+ backDate.getDate()).slice(-2)}`);
+    buttonBack.setAttribute('data-date',`${backDate.getFullYear()}-${('0'+ (backDate.getMonth()+1)).slice(-2)}-${('0'+ backDate.getDate()).slice(-2)}`);
 }
 renderCalendar();
-
+//funkcja tworzenia pustych okien w kalendarzu
 function renderEmptyDivs(firstDay){
     const weekDay = firstDay.getDay();
     const emptyDivNumber = weekDay ? weekDay - 1 : 6;
     for (let i = 0; i < emptyDivNumber; i++){
         const emptyDiv = document.createElement('div');
-        emptyDiv.className = "single_day";
+        emptyDiv.className = 'single_day';
         singleDayContainer.append(emptyDiv);
     }
 } 
+//PD.zmienić adresowanie po id na klasę event-time i event-content
+//funckja tworzenia przycisku do edycji 
+function createEditButton(index){
+    const buttonEdit = document.createElement('button');
+    buttonEdit.setAttribute('class','edit');
+    console.log('index',index);
+    buttonEdit.setAttribute('data-index',index.toString());
+    buttonEdit.addEventListener('click',function(event){
+        event.preventDefault();
+        const parent = event.target.parentElement;
+        const timeTask = parent.querySelector('.event-time');
+        timeTask.contentEditable = true;
+        timeTask.style.backgroundColor = '#fff';
+        const span = parent.querySelector('.event-content');
+        span.contentEditable = true;
+        span.style.backgroundColor = '#fff';
+        event.target.className = event.target.className + ' to_remove';
+        replaceEditWithSaveEvent.lineIndex = index;
+        document.dispatchEvent(replaceEditWithSaveEvent);
+    })
+    return buttonEdit;
+}
+//nasłuchiwanie na kliknięcie pojedyńczego dnia w miesiącu 
 singleDayContainer.addEventListener('click',function(event){
-    if(event.target.className.includes("proper_day")){
-        task.className = "task add_task";
-        const date = event.target.dataset.date;
-        taskDateElement.textContent = date;
-        const existingEvents = getFromLocalStorageByDate(date);
-        todoListContainer.innerHTML = '';
-        Object.keys(existingEvents).forEach((time)=>{
-            const event = existingEvents[time];
-            const li = document.createElement("li");
-            const timeTask = document.createElement("span");
-            //const label = document.createElement("label");
-            const buttonEdit = document.createElement("button");
-            const span = document.createElement("span");
-            const checkobox = document.createElement("input");
-            timeTask.setAttribute('id', 'event-time');
-            timeTask.setAttribute('data-time', event.time);
-            timeTask.textContent = event.time;
-            span.textContent = event.contents;
-            span.setAttribute('id','event-content');
-            buttonEdit.setAttribute('class','edit');
-            checkobox.setAttribute('type','checkbox');
-            if(event.done){
-               checkobox.setAttribute('checked',event.done);
-            }
-            checkobox.addEventListener('click',function(event){
-                const dateEvents = getFromLocalStorageByDate(date);
-                const currentEvent = dateEvents[time];
-                const done = event.target.checked;
-                const newCurrentEvent = {
-                    ...currentEvent,
-                    done
-                }
-                const newDateEvents = {
-                    ...dateEvents,
-                    [time]: newCurrentEvent
-                }
-                localStorage.setItem(date,JSON.stringify(newDateEvents));
-            })
-            buttonEdit.addEventListener('click',function(event){
-                event.preventDefault();
-                const timeTask = document.getElementById('event-time');
-                timeTask.contentEditable = true;
-                timeTask.style.backgroundColor = "#fff";
-                const span = document.getElementById('event-content');
-                span.contentEditable = true;
-                span.style.backgroundColor = "#fff";
-                event.target.className = event.target.className + " to_remove";
-                document.dispatchEvent(replaceEditWithSaveEvent);
-            })
-           // label.append(checkobox);
-           // label.append(span);
-            li.append(timeTask);
-            //li.append(label);
-            li.append(checkobox);
-            li.append(span);
-            li.append(buttonEdit);
-            todoListContainer.append(li);
-        })
+    if(event.target.className.includes('proper_day')){
+        location.search = '?currentDate='+event.target.dataset.date;
     }
 })
+//otwieranie okna z terminarzem na wybrany dzień miesiąca
+function openDayEdit(date){
+    console.log(date);
+    task.className = 'task add_task';
+    taskDateElement.textContent = date;
+    const existingEvents = getFromLocalStorageByDate(date);
+    todoListContainer.innerHTML = '';
+    Object.keys(existingEvents).forEach((time,index)=>{
+        const event = existingEvents[time];
+        const li = document.createElement('li');
+        const timeTask = document.createElement('span');
+        //const label = document.createElement('label');
+        const buttonEdit = createEditButton(index);
+        const span = document.createElement('span');
+        const checkobox = document.createElement('input');
+        timeTask.className ='event-time';
+        timeTask.setAttribute('data-time', event.time);
+        timeTask.textContent = event.time;
+        span.textContent = event.contents;
+        span.className ='event-content';
+        checkobox.setAttribute('type','checkbox');
+        if(event.done){
+            checkobox.setAttribute('checked',event.done);
+        }
+        checkobox.addEventListener('click',function(event){
+            const dateEvents = getFromLocalStorageByDate(date);
+            const currentEvent = dateEvents[time];
+            const done = event.target.checked;
+            const newCurrentEvent = {
+                ...currentEvent,
+                done
+            }
+            const newDateEvents = {
+                ...dateEvents,
+                [time]: newCurrentEvent
+            }
+            localStorage.setItem(date,JSON.stringify(newDateEvents));
+        })
+        // label.append(checkobox);
+        // label.append(span);
+        li.append(timeTask);
+        //li.append(label);
+        li.append(checkobox);
+        li.append(span);
+        li.append(buttonEdit);
+        todoListContainer.append(li);
+    })
+}
+//nasłuchiwanie na kliknięcie w przycisk do zamykania okna z terminarzem
 buttonClose.addEventListener('click',function(event){
     task.className = 'task';
 })
+//nasłuchiwanie na kliknięcie w przycisk ADD
 buttonAdd.addEventListener('click',function(event){
+    event.preventDefault();
     const date = taskDateElement.textContent;
     console.log('date',date);
     const taskTime = input.value;
@@ -214,7 +238,7 @@ buttonAdd.addEventListener('click',function(event){
         formDataObject[value[0]] = value[1];
     }
     if(!taskTime){
-        alert("nie podano godziny")
+        alert('nie podano godziny')
         return;
     }
     console.log(JSON.stringify(formDataObject));
@@ -222,8 +246,8 @@ buttonAdd.addEventListener('click',function(event){
     console.log(existingEvents);
 
     if (existingEvents[formDataObject.time]){
-        if(!window.confirm("Wydarzenie o godzinie " + formDataObject.time + " już istnieje, czy chcesz nadpisać?")){
-            console.log("zapisz dane do localStorage");
+        if(!window.confirm('Wydarzenie o godzinie ' + formDataObject.time + ' już istnieje, czy chcesz nadpisać?')){
+            console.log('zapisz dane do localStorage');
             return;
         }
     }
@@ -237,3 +261,17 @@ function getFromLocalStorageByDate(date){
     const existingEvents = storageElement ? JSON.parse(storageElement) : {};
     return existingEvents;
 }
+
+window.addEventListener('load',function(){
+    const currentDate = (new URLSearchParams(location.search)).get('currentDate');
+    console.log(currentDate,typeof currentDate)
+    if(currentDate){
+        openDayEdit(currentDate);
+    }
+})
+
+
+//znaleźć gdzie wołam funkcję bez przekazania indexu i przekazać go
+//zrobić commit
+
+// pobawić się z window.location
